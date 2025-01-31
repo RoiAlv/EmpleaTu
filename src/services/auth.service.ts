b11338495083f0e25c77ef454bc216f43c98e5a7
RoiAlv <Roi040205@gmail.com>
2025-01-31 09:35:42 +0100

    sgdf

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,25 +1,56 @@
 import { HttpException } from "../exceptions/httpException";
 import { PrismaClient, User } from "@prisma/client";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+
+// usar un patron: singlenton
 const prisma = new PrismaClient()
+const TOKEN_PASSWORD = process.env.TOKEN_PASSWORD || 'pass'
 
-export class UserService {
-    static async getByEmail(email: string){
-       const findUser = await prisma.user.findUnique(
-        { where: {email}, omit: {password:true}}
-        )
-       if(!findUser) throw new HttpException(404,'User not found')
-        return findUser
-    }
+export class AuthService {
+    static async register(user: User) {
+        // ver si el usuario no existe
+        // SELECT id,nombre FROM user WHERE email=user.email
+        const findUser = await prisma.user.findUnique({where: {email: user.email}})
+        if (findUser) throw new HttpException(409, `User ${user.email} already exists`)
 
-    static async getById(id: number){
-        const findUser = await prisma.user.findUnique({ where: {id}})
-        if(!findUser) throw new HttpException(404, 'User not found')
-         return findUser
-     }
-    static async getAll(){
-        const users = await prisma.user.findMany({
-            omit: {password:true}
+        // encriptar el password
+        const passwordEncrypted = await bcrypt.hash(user.password, 10)
+        user.password=''
+        // guardar el usuario en la bd
+        // INSERT INTO user (name, password, email) VALUES (?,?,?)
+        return await prisma.user.create({
+            data:{
+                ...user,
+                password: passwordEncrypted,
+                role: null
+            },
+            omit:{
+                password:true
+            }
         })
-        return users
     }
+
+    static async login(email:string, password:string){
+         // ver si el usuario existe
+        //const query = `SELECT id, email, role, password FROM user WHERE email='${email}'`
+        //const findUsers = await prisma.$queryRawUnsafe(query) as User[]
+        //const findUser = findUsers[0]
+
+        const findUser = await prisma.user.findUnique({where:{email}})
+        if(!findUser) throw new HttpException(401, 'Invalid user or password')
+         // ver si el password coincide
+        const isPasswordCorrect = await bcrypt.compare(password, findUser.password)
+        if(!isPasswordCorrect) throw new HttpException(401,'Invalid user or password')
+
+        // generar el token de autenticación
+        const token = jwt.sign(
+            {colorFavorito:'azul', id:findUser.id, email:findUser.email, role:findUser.role}, 
+            TOKEN_PASSWORD, 
+            {expiresIn:"1h"}
+        )
+        // devolver el token
+        return token
+    }
+
 }
